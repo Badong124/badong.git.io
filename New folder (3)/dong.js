397b6ab1ca0b80141892461aeefce47392dbb2b2
Badong124 <122802023+Badong124@users.noreply.github.com>
2023-01-17 00:04:39 +0700

    Add files via upload

---
+++ New folder (3)/dong.js
@@ -0,0 +1,6 @@
+let int = `${x , y}`;
+if ( x > y);
+alert ("isGreater")
+esle
+alert ("isSmaller");
+
---
+++ New folder (3)/main.js
@@ -0,0 +1,9 @@
+let int = `${x, y}`;
+ if ( x >= y){
+ alert ( `x is greater than y`);
+ alert ()
+ }
+ else{
+ alert (`y is greater than x`);
+ alert (`false`);
+ }
